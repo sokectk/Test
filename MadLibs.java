89be97feef2cb764d2346a51738fbccf13ff030b
PC-20220419YCNW\Administrator <caojielyg@gmail.com>
2022-12-13 00:45:48 +0800

    1212-Learn Java:Variables

---
+++ MadLibs.java
@@ -0,0 +1,37 @@
+public class MadLibs {
+  /*
+  This program generates a mad libbed story.
+  Author: Laura
+  Date:2/19/2049
+  */
+  	public static void main(String[] args){
+      String name1 = "Angle";
+      String adjective1 = "light";
+      String adjective2 = "red";
+      String adjective3 = "big";
+      String verb1 = "is";
+      String noun1 = "apple";
+      String noun2 = "pear";
+      String noun3 = "sea";
+      String noun4 = "boat";
+      String noun5 = "sun"; 
+      String noun6 = "moon";
+      String name2 = "Bob";
+      int number = 10;
+      String place1 = "TOKYO";      
+      
+
+
+
+
+
+
+
+
+
+      
+      //The template for the story
+      String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+      System.out.println(story);
+    }       
+}
---
+++ MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name = "cj";
+    int age = 30;
+    double desiredSalary = 53422.21;
+    char gender = 'm';
+    boolean lookingForJob = true;
+	}
+}
